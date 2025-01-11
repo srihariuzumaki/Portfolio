51f5ea2b40c0c42d285235a4ba7c92c4dd22dc92
srihari <srihariamit@gmail.com>
2025-01-11 21:58:06 +0530

    initial design

---
+++ src/constants/index.js
@@ -0,0 +1,112 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  
+  {
+    id: 3,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+
+
+export const myProjects = [
+  {
+    title: 'Task Mate - Ai Study planner and Material Organizer',
+    desc: 'Task Mate is a Web Application that helps users manage their daily tasks and study materials efficiently. It leverages the power of OpenAI to generate personalized study plans and materials based on user inputs and Users can store and access their study materials in a user-friendly interface.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Firebase , Task Mate is designed for optimal performance and scalability.',
+    href: 'https://taskmate-007.vercel.app/',
+    texture: '/textures/project/Project_1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'JARVIS - Personal Assistant and Study Mentor',
+    desc: 'JARVIS is a powerful Chatbot that can act as a Personal Assistant and Study Mentor with Voice Assistant..',
+    subdesc:
+      'With JARVIS, User can chat with the bot and ask about their acadamic needs and user can also upload materials and the bot will give you the summary of the particular file. and it has also voice feature that can be used to interact with the bot.',
+    href: 'https://jarvis-ten-sage.vercel.app/',
+    texture: '/textures/project/Project_2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+  
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+
+
