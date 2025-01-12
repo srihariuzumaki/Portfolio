342d685a442eaaff7584445a8d896dbf9b1e2955
srihari <srihariamit@gmail.com>
2025-01-12 12:53:26 +0530

    added the logo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Srihari<span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">An aspiring Frontend Web Developer</p>
+        <p className="hero_tag text-gray_gradient">An aspiring Web Developer</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
